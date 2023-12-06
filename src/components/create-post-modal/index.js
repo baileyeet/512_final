816a699ed600bfc26c380ab1d25aace7ba49e40b
emartinez77 <emartinez0044@gmail.com>
2023-12-05 20:25:17 -0500

    community base 2

--- src/components/create-post-modal/index.js
+++ src/components/create-post-modal/index.js
@@ -3,13 +3,17 @@ import style from "./createModal.module.scss";
 import SymptomTable from "../symptom-table"
 import {useTheme} from "../theme-color";
 
-const CreatePostModal = ({ isOpen, onClose }) => {
+import brainIcon from "../../assets/community-brainIcon.svg";
+import dropDownIcon from "../../assets/community-dropDownIcon.svg"
+
+const CreatePostModal = ({ isOpen, onClose, startingCategory }) => {
     const { theme} = useTheme();
   if (!isOpen) return null;
   
   const postBGColor = {background: theme === "yellow" ? "rgba(255, 197, 98, 0.50)" : 
                                 theme === "blue" ? "rgba(66, 95, 128, 0.50)" :
                                 "rgba(246, 162, 144, 0.50)"}
+  const category = startingCategory ? startingCategory : "Mental Health";
   return (
     <div className="modal">
       <div className="modal-content">
@@ -18,7 +22,12 @@ const CreatePostModal = ({ isOpen, onClose }) => {
                 <div className={style.title}>
                     Create a Post
                 </div>
-                <div className={style.boxes}>
+                <div className={style.content}>
+                    <div className={style.categoryRow}>
+                        <img alt= "icon" src={brainIcon}></img>
+                        <p>{category}</p>
+                        <img alt="icon" src={dropDownIcon} style = {{width : "24px", height : "24px", alignSelf: "flex-end", cursor:"pointer"}}></img>
+                    </div>
                     <div className={style.titleBox}>
                         <p>Title...</p>
                     </div>
